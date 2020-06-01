1205ec38318075335201be4c55ba6a3bc16f1d94
Mohomed Rimash <rimash@yaalalabs.com>
2020-06-02 05:27:26 +0530

    Updates

---
+++ precache-manifest.b5ac531a0ad76cfcec347d7004f5811e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d17c9ea1e82910842f965796b06d96",
+    "url": "/react-reactstrap-sandbox/index.html"
+  },
+  {
+    "revision": "68b15b1d2a29010d4759",
+    "url": "/react-reactstrap-sandbox/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7e14e9446185f1633437",
+    "url": "/react-reactstrap-sandbox/static/css/main.f0d091f3.chunk.css"
+  },
+  {
+    "revision": "68b15b1d2a29010d4759",
+    "url": "/react-reactstrap-sandbox/static/js/2.8167d194.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/react-reactstrap-sandbox/static/js/2.8167d194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e14e9446185f1633437",
+    "url": "/react-reactstrap-sandbox/static/js/main.e2f528ee.chunk.js"
+  },
+  {
+    "revision": "692e9a9c980ef1f9ce64",
+    "url": "/react-reactstrap-sandbox/static/js/runtime-main.ab9a60f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-reactstrap-sandbox/static/media/logo.5d5d9eef.svg"
+  }
+]);
